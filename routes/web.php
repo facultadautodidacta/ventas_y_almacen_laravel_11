b1259676c63f9fe2a7c7757ad5674febe4325d3f
Facultad Autodidacta <33157200+facultadautodidacta@users.noreply.github.com>
2025-02-17 22:06:43 -0600

    Menu con rutas de los modulos

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,17 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Dashboard;
+use App\Http\Controllers\DetalleVentas;
+use App\Http\Controllers\Ventas;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'index'])->name('login');
 Route::get('/home', [Dashboard::class, 'index'])->name('home');
+
+Route::prefix('ventas')->group(function(){
+    Route::get('/nueva-venta', [Ventas::class, 'index'])->name('ventas-nueva');
+});
+
+Route::prefix('detalle')->group(function(){
+    Route::get('/detalle-venta', [DetalleVentas::class, 'index'])->name('detalle-venta');
+});
